9b17ee02ce14f6cdec7abbfa57a255275472fc46
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-11-22 08:48:33 +0100

    propriété de tableau de prix selon packaging mis sur inline

--- view/catalog_view.php
+++ view/catalog_view.php
@@ -8,16 +8,16 @@
             $this->catalogView.="<section class='showcase'>";
             foreach ($showroom->Products as $product){
                 //Chaque produit
-                $this->catalogView.="<div class='product'>".$product->année." ".$product->product;
+                $this->catalogView.="<div class='product'>".$product->année." ".$product->product."<div class='prices'>";
                 
                 foreach($product->packaging as $packaging){
                     //Les prix selon la quantité
-                    $this->catalogView.="<table class='prices'>";
-                    $this->catalogView.="<thead class='packaging'><tr><th colspan='2'>".$packaging->quantité."</th></tr></thead>";
-                    $this->catalogView.="<tbody><tr><td class='price'>".$packaging->price."</td><td>la bouteille</td></tr>";
+                    $this->catalogView.="<table class='packaging'>";
+                    $this->catalogView.="<thead class='quantite'><tr><th colspan='2'>".$packaging->quantité."</th></tr></thead>";
+                    $this->catalogView.="<tbody><tr><td class='price'>".$packaging->price."</td><td>l'unité</td></tr>";
                     $this->catalogView.="</tbody></table>";
                 };
-                $this->catalogView.="</div>";
+                $this->catalogView.="</div></div>";
             };
 
             $this->catalogView.="</section>";
