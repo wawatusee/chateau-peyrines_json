b578ddb29076b27da7dc23d73d6759d3556a07f1
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-12-06 08:36:56 +0100

    Tableau des dates alimenté pas tournee.json

--- view/tour_view.php
+++ view/tour_view.php
@@ -1,10 +1,19 @@
 <?php class TourView{
  private $tourView=" ";
     public function getView(array $tourArray){
+        $this->tourView.="<table><thead>Tournée</thead>";
         foreach($tourArray as $date){
             //Affichage de chaque date d'event
-            $this->tourView.="<h3>".$date->date."</H3>";
+            $this->tourView.="<tr>";
+            $this->tourView.="<td>".$date->date."</td>";
+            $this->tourView.="<td>".$date->location."</td>";
+            $this->tourView.="<td>".$date->type."</td>";
+            if ($date->text){
+                $this->tourView.="<td>".$date->text."</td>";
+            }
+            $this->tourView.="</tr>";
         }
+        $this->tourView.="</table>";
         return $this->tourView;
     }
 
