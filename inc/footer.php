8bf467ab7ad383025e3fddf76a38c14eab67ffcd
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-12-14 13:27:39 +0100

    Design du footer, doublons retirés de l'index(tag nav, tag footer, tag header)

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,14 @@
 <footer>
     <div class="footerNav">
-        <nav class="mainNav">
+        <nav class="navfooterbloc">
             <h4>Menu principal</h4>
             <?php echo $menuMain_view?>
         </nav>
-        <nav class="extNav">
-            <h4>Liens extérieurs</h4>
-            Liens extérieurs
+        <nav class="navfooterbloc">
+            <h4>Ils en parlent</h4>
+            <a href="https://youtu.be/h3RULNKo_wo" target="_blank" rel="noopener noreferrer">TASTED by Andreas Larsson</a>
+            <a href="https://www.hachette-vins.com/guide-vins/producteurs/47281/scea-behaghel-ch-peyrines/" target="_blank" rel="noopener noreferrer">Hachette vins</a>
+            <a href="https://www.concourslyon.com/wine/33873/chateau-peyrines" target="_blank" rel="noopener noreferrer">Concours international de lyon</a>
         </nav>
     </div>
     <nav id="menuRS" class="nav-rs">
