0ec19ad80419511de897f8dccea274ac0ac17945
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-12-22 10:40:19 +0100

    grid centre main

--- inc/footer.php
+++ inc/footer.php
@@ -9,6 +9,7 @@
             <a href="https://youtu.be/h3RULNKo_wo" target="_blank" rel="noopener noreferrer">TASTED by Andreas Larsson</a>
             <a href="https://www.hachette-vins.com/guide-vins/producteurs/47281/scea-behaghel-ch-peyrines/" target="_blank" rel="noopener noreferrer">Hachette vins</a>
             <a href="https://www.concourslyon.com/wine/33873/chateau-peyrines" target="_blank" rel="noopener noreferrer">Concours international de lyon</a>
+            <a href="https://agriculture.gouv.fr/la-haute-valeur-environnementale-une-mention-valorisante-pour-les-agriculteurs-et-leurs-pratiques" target="_blank" rel="noopener noreferrer">Haute valeur environnementale</a>
         </nav>
     </div>
     <nav id="menuRS" class="nav-rs">
--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
      crossorigin=""/>
@@ -21,7 +21,7 @@
         <?php require_once('../inc/nav.php')?>
     </div>
     <main role=main>
-        <section id="accueil" style="padding-left:16px">
+        <section id="accueil">
             <h1>Accueil</h1>
             <article>
                 <h2>Présentation du site</h2>
