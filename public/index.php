9460483bd975ce366e1e540b97efa352b74c737e
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-12-28 01:37:11 +0100

    Intégration de la balise meta google pour prouver que google search

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="00xcLmYAyZXyMNUqTeggzhT2lDVH5gSZxB2BCkXpu4A" />
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
