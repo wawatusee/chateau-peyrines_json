a388471cec0c63f11ac66372dfe7e66a6420ac37
wawatusee <web2020.kieran.labarrere@gmail.com>
2022-12-30 17:36:26 +0100

    background header l'image passe du premier plan à l'arriere plan au retrecissement d'écran

--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,3 @@
 <header>
-    <h1>Chateau<img class="logo" src="img/main-logo.png" alt="logo-site">Peyrines<span><?php echo $titleWebSite[1]?></span></h1>
+    <h1><?php echo $titleWebSite[0]?><img class="logo" src="img/main-logo.png" alt="logo-site"><?php echo " ".$titleWebSite[1]?><span><?php echo $titleWebSite[2]?></span></h1>
 </header>
