b16c9e1c9ce1eda22f750e3ed9b47d4447a6234f
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-01-20 17:47:03 +0100

    inc footer.php titre dans rs

--- public/index.php
+++ public/index.php
@@ -102,13 +102,13 @@
         <section id="contact">
             <h2>Contact</h2>
             <div class="presentation">
-            <a href="https://goo.gl/maps/WXbuRqfbw21NUYKFA"><img src="/public/img/picto-map" alt="picto-adresse"></a><span> Chateau Peyrines, 33410 Mourens, France <br>Visite & dégustation sur place</span>
+            <a href="https://goo.gl/maps/WXbuRqfbw21NUYKFA"><img src="/public/img/picto-map-rouge.png" alt="picto-adresse"></a><span> Chateau Peyrines, 33410 Mourens, France <br>Visite & dégustation sur place</span>
             </div>
             <div class="presentation">
-                <a href="+33055661905"><img src="/public/img/picto-phone" alt="picto-téléphone"></a><span>+33 05 56 61 98 05</span>
+                <a href="+33055661905"><img src="/public/img/picto-phone-rouge.png" alt="picto-téléphone"></a><span>+33 05 56 61 98 05</span>
              </div>
             <div class="presentation">
-                <a href="mailto:contact@chateau-peyrines.com"><img src="/public/img/picto-mail" alt="picto-mail"></a><span>contact@chateau-peyrines.com</span>
+                <a href="mailto:contact@chateau-peyrines.com"><img src="/public/img/picto-mail-rouge" alt="picto-mail"></a><span>contact@chateau-peyrines.com</span>
             </div>
             
         </section>
