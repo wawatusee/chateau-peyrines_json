992cc03408bf967a26e6494b4e5a73bfe30792c0
wawatusee <web2020.kieran.labarrere@gmail.com>
2023-03-27 19:32:08 +0200

    Changement liens acceuil

--- inc/pages/accueil.php
+++ inc/pages/accueil.php
@@ -5,16 +5,16 @@
     <section>
         <h3>Présentation du site</h3>
         <div class="presentation">
-            <a href="#accueil" class="lien"><img class="picto-lien" src="/public/img/picto-accueil.png" alt="dessin du chateau">Accueil</a><span>Bonjour à toi citoyen de plus de 18 ans, bienvenu sur le site de Château Peyrines.<br> Château Peyrines est appelé château car sur la terre où poussent nos vignes aujourd'hui, autrefois il y avait un château, celui des comtes de Peyrines. Quelques pierres sont encore visibles et les bouteilles de vin, pour le souvenir sont estampillées du blason de ces résidents féodaux. Nommer notre vignoble "Château", convenons, si vous le voulez bien que l'usage soit plutôt agréable.<br>
+            <a href="?page=accueil" class="lien"><img class="picto-lien" src="/public/img/picto-accueil.png" alt="dessin du chateau">Accueil</a><span>Bonjour à toi citoyen de plus de 18 ans, bienvenu sur le site de Château Peyrines.<br> Château Peyrines est appelé château car sur la terre où poussent nos vignes aujourd'hui, autrefois il y avait un château, celui des comtes de Peyrines. Quelques pierres sont encore visibles et les bouteilles de vin, pour le souvenir sont estampillées du blason de ces résidents féodaux. Nommer notre vignoble "Château", convenons, si vous le voulez bien que l'usage soit plutôt agréable.<br>
         Sur le site internet de Château-Peyrines.com vous trouverez un sobre descriptif de Château-Peyrines,  le catalogue des vins disponibles à la commande, les dates et lieux de la tournée et les divers moyens de contacter les acteurs de ce domaine.
         </span>
         </div>
         <div class="presentation">
-            <a href="#catalogue" class="lien"><img class="picto-lien" src="/public/img/picto-bouteille.png" alt="dessin de bouteille de vin">Catalogue</a><span>Le catalogue présente tous les vins de Château-Peyrines que vous pouvez commander : vin blanc sec, vin rouge supérieur, vin moelleux, bulles de Peyrines, vin rosé.
+            <a href="?page=catalogue" class="lien"><img class="picto-lien" src="/public/img/picto-bouteille.png" alt="dessin de bouteille de vin">Catalogue</a><span>Le catalogue présente tous les vins de Château-Peyrines que vous pouvez commander : vin blanc sec, vin rouge supérieur, vin moelleux, bulles de Peyrines, vin rosé.
             Le catalogue présente les tarifs de chacun de ces vins, selon qu'ils soient livrés ou achetés sur place, et adaptés aux quantités commandées.</span>
         </div>
         <div class="presentation">
-            <a href="#tournee" class="lien"><img class="picto-lien" src="/public/img/picto-tourne.png" alt="dessin de camionette">Tournée</a><span>Nous déplaçons avec une camionette quelques unes de nos bouteilles à divers endroits en France à divers moments <br>
+            <a href="?page=tournee" class="lien"><img class="picto-lien" src="/public/img/picto-tourne.png" alt="dessin de camionette">Tournée</a><span>Nous déplaçons avec une camionette quelques unes de nos bouteilles à divers endroits en France à divers moments <br>
             Dégustations-vente, livraison et autres déplacements ci-dessus un lien vers les dates et lieux de cette tournée annuelle en France.</span>
         </div>
     </section>
